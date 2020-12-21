94ed8b1f6b5a8643f41e4e0c31b0f1805cb6aebe
Jayant Jha <Jayantanandjha4186@gmail.com>
2020-12-21 22:13:29 +0530

    Changes

---
+++ app/src/main/java/com/manipal/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.manipal;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+public class SplashActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent login = new Intent(SplashActivity.this, LoginActivity.class);
+            login.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(login);
+            finish();
+//                Intent mainIntent = new Intent(SplashActivity.this, LoginActivity.class);
+//                SplashActivity.this.startActivity(mainIntent);
+//                SplashActivity.this.finish();
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
