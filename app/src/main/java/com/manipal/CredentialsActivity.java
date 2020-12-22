f2d8f9c5d5e7887d0012c756df1bad14140f6730
Jayant Jha <Jayantanandjha4186@gmail.com>
2020-12-22 19:47:21 +0530

    Authentication Function added

--- app/src/main/java/com/manipal/CredentialsActivity.java
+++ app/src/main/java/com/manipal/CredentialsActivity.java
@@ -1,14 +1,104 @@
 package com.manipal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class CredentialsActivity extends AppCompatActivity {
+    TextInputEditText mUsername, mName, mEmail, mPassword, mDepartment;
+    Button regBtn;
+    TextView certcred;
+    ProgressDialog pd;
+    FirebaseAuth mAuth;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credentials);
+        mUsername = findViewById(R.id.usernametext);
+        mEmail = findViewById(R.id.emailtext);
+        mPassword = findViewById(R.id.passwordtext);
+        mName = findViewById(R.id.nametext);
+        mDepartment = findViewById(R.id.departmenttext);
+        regBtn = findViewById(R.id.regBtn);
+        mAuth = FirebaseAuth.getInstance();
+        certcred = findViewById(R.id.certifiedcredentials);
+        certcred.setOnClickListener(v -> {
+            Intent login = new Intent(CredentialsActivity.this, LoginActivity.class);
+            login.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(login);
+        });
+        regBtn.setOnClickListener(v -> {
+            pd = new ProgressDialog(CredentialsActivity.this);
+            pd.setMessage("Please Wait..");
+            pd.show();
+
+            String str_username = mUsername.getText().toString().trim();
+            String str_name = mName.getText().toString().trim();
+            String str_email = mEmail.getText().toString().trim();
+            String str_password = mPassword.getText().toString().trim();
+            String str_department = mDepartment.getText().toString().trim();
+
+            if (TextUtils.isEmpty(str_username)||TextUtils.isEmpty(str_email)||TextUtils.isEmpty(str_name)||TextUtils.isEmpty(str_password)||TextUtils.isEmpty(str_department)){
+                pd.dismiss();
+                Toast.makeText(CredentialsActivity.this, "All field are required..", Toast.LENGTH_LONG).show();
+            }else if (str_password.length() < 6){
+                pd.dismiss();
+                Toast.makeText(CredentialsActivity.this,"Password must be 6 characters long..",Toast.LENGTH_LONG).show();
+            }else {
+                register(str_username,str_department,str_email,str_password,str_name);
+            }
+        });
+    }
+
+    private void register(String username, String department, String email, String password, String name) {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+                FirebaseUser firebaseUser = mAuth.getCurrentUser();
+                String userUid = firebaseUser.getUid();
+                reference = FirebaseDatabase.getInstance().getReference().child("Users").child(userUid);
+                HashMap<String, Object> hashMap = new HashMap<>();
+                hashMap.put("id",firebaseUser.getUid());
+                hashMap.put("name",name);
+                hashMap.put("username",username);
+                hashMap.put("department", department);
+                hashMap.put("email", email);
+                hashMap.put("imageURL","https://firebasestorage.googleapis.com/v0/b/vani-chat-9b86a.appspot.com/o/profile.jpg?alt=media&token=9f42e809-127c-49e2-9a53-351cd8d38104");
+                reference.setValue(hashMap).addOnCompleteListener(task1 -> {
+                    if (task1.isSuccessful()){
+                        pd.dismiss();
+                        Intent main = new Intent(CredentialsActivity.this, MainActivity.class);
+                        main.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        main.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(main);
+                        finish();
+                    }else {
+                        pd.dismiss();
+                        Toast.makeText(CredentialsActivity.this, "Something went wrong! Please try later", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
     }
 }
--- app/src/main/java/com/manipal/LoginActivity.java
+++ app/src/main/java/com/manipal/LoginActivity.java
@@ -1,36 +1,89 @@
 package com.manipal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
-    TextInputLayout mUsername, mEmail, mPassword;
+    TextInputEditText mUsername, mEmail, mPassword;
     Button loginBtn;
     TextView certcred;
+    ProgressDialog pd;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mUsername = findViewById(R.id.username);
-        mEmail = findViewById(R.id.email);
-        mPassword = findViewById(R.id.password);
+        mUsername = findViewById(R.id.usernametext);
+        mEmail = findViewById(R.id.emailtext);
+        mPassword = findViewById(R.id.passwordtext);
         loginBtn = findViewById(R.id.loginBtn);
+        mAuth = FirebaseAuth.getInstance();
         certcred = findViewById(R.id.certifiedcredentials);
         certcred.setOnClickListener(v -> {
             Intent credIntent = new Intent(LoginActivity.this, CredentialsActivity.class);
             credIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(credIntent);
+        });
+        loginBtn.setOnClickListener(v -> {
+            pd = new ProgressDialog(LoginActivity.this);
+            pd.setMessage("Please Wait..");
+            pd.show();
+            String str_username = mUsername.getText().toString();
+            String str_email = mEmail.getText().toString().trim();
+            String str_password = mPassword.getText().toString().trim();
+            if (TextUtils.isEmpty(str_email)||TextUtils.isEmpty(str_password)||TextUtils.isEmpty(str_username)){
+                pd.dismiss();
+                Toast.makeText(LoginActivity.this,"All fields are required..",Toast.LENGTH_LONG).show();
+            }else {
+                mAuth.signInWithEmailAndPassword(str_email,str_password).addOnCompleteListener(LoginActivity.this, task -> {
+                    if (task.isSuccessful()){
+                        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Users").child(mAuth.getCurrentUser().getUid());
+                        reference.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                pd.dismiss();
+                                Intent main = new Intent(LoginActivity.this, MainActivity.class);
+                                main.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(main);
+                                finish();
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                                pd.dismiss();
+                            }
+                        });
+                    }
+                    else {
+                        pd.dismiss();
+                        Toast.makeText(LoginActivity.this,"Authentication Failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
         });
     }
 
