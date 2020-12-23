192449a9abaf8dba9ec49ea270d48449a3ad5321
Jayant Jha <Jayantanandjha4186@gmail.com>
2020-12-24 01:19:47 +0530

    Search Function Added

--- app/src/main/java/com/manipal/MainActivity.java
+++ app/src/main/java/com/manipal/MainActivity.java
@@ -14,7 +14,11 @@ import android.os.Vibrator;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -26,6 +30,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.manipal.Model.User;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
@@ -35,7 +40,13 @@ public class MainActivity extends AppCompatActivity {
     TextView status;
     String uID;
     Dialog dialog;
-    ImageView fi,si,pi,ri,anr,fui,pri,smi,logoutBtn;
+    RelativeLayout rl;
+    ListView lv;
+    SearchView searchView;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> list;
+    //String[] data = {"Giriraj Methi","Nilanjan Halder", "Alok Damre", "Prakash Ramani", "Prabhu Inbraj", "Tej Pal"};
+    ImageView fi,si,pi,ri,anr,fui,pri,smi,logoutBtn,close;
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,9 +57,13 @@ public class MainActivity extends AppCompatActivity {
         mCurrentUser = mAuth.getCurrentUser();
         uID = mCurrentUser.getUid();
         fi = findViewById(R.id.fi);
+        rl = findViewById(R.id.info);
+        lv = findViewById(R.id.listView);
+        searchView = findViewById(R.id.searchBar);
         si = findViewById(R.id.si);
         pi = findViewById(R.id.pi);
         ri = findViewById(R.id.ri);
+        close = findViewById(R.id.close);
         anr = findViewById(R.id.anr);
         dialog = new Dialog(MainActivity.this);
         dialog.setContentView(R.layout.check_layout);
@@ -57,13 +72,60 @@ public class MainActivity extends AppCompatActivity {
         dialog.setCancelable(false);
        // dialog.getWindow().getAttributes().windowAnimations = R.style.animation;
         fui = findViewById(R.id.fui);
-        logoutBtn = (ImageView) findViewById(R.id.logout);
+        logoutBtn = findViewById(R.id.logout);
         pri = findViewById(R.id.pri);
         smi = findViewById(R.id.smi);
+        list = new ArrayList<String>();
+        list.add("Giriraj Methi");
+        list.add("Nilanjan Halder");
+        list.add("Alok Damre");
+        list.add("Prakash Ramani");
+        list.add("Prabhu Inbaraj");
+        list.add("Tej Pal");
+        list.add("Pushpendra Kumar");
+        list.add("Vishal Das");
+        list.add("Rashi Nathawat");
+        list.add("Naveen Kumar Singh");
+        list.add("Sourav Kumar Das");
+        list.add("Reenu Gill");
+        list.add("Vikram Singh Kashyap");
+        list.add("Bhoopendra Panchri");
+        list.add("Dr. Dinesh Yadav");
+        list.add("Mahesh Kumar Dubey");
+        list.add("Lav Maheshwari");
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+        lv.setAdapter(adapter);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                rl.setVisibility(View.GONE);
+                lv.setVisibility(View.VISIBLE);
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
         Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
         final long[] pattern = {40, 80};
         TextView okay = dialog.findViewById(R.id.okayBtn);
         TextView cancel = dialog.findViewById(R.id.cancelBtn);
+        TextView title = dialog.findViewById(R.id.headerText);
+        TextView message = dialog.findViewById(R.id.confirmText);
+//        searchView.setOnClickListener(v -> {
+//            vibrator.vibrate(pattern, -1);
+//            rl.setVisibility(View.GONE);
+//            lv.setVisibility(View.VISIBLE);
+//        });
+        close.setOnClickListener(v -> {
+            vibrator.vibrate(pattern, -1);
+            rl.setVisibility(View.VISIBLE);
+            lv.setVisibility(View.GONE);
+            searchView.setFocusable(false);
+        });
         okay.setOnClickListener(v -> {
             vibrator.vibrate(pattern, -1);
             dialog.dismiss();
@@ -78,6 +140,10 @@ public class MainActivity extends AppCompatActivity {
             dialog.dismiss();
         });
         logoutBtn.setOnClickListener(v -> {
+            title.setVisibility(View.VISIBLE);
+            title.setText("Log Out");
+            message.setVisibility(View.VISIBLE);
+            message.setText("Are you sure you want to Log Out?");
             vibrator.vibrate(pattern, -1);
             dialog.show();
 //            vibrator.vibrate(pattern, -1);
